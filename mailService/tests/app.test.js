188ded85c9cc46b082934b4bb218ce7948570f69
RaulMF40 <raulmf.30@gmail.com>
2024-10-13 22:37:19 +0200

    resuelto

--- mailService/tests/app.test.js
+++ mailService/tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../src/app')
+const app = require('../src/app') // Ajusta la ruta si es necesario
 
 describe('API Tests', () => {
   it('should return Hello World on GET /', async () => {
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-
-const PORT = 3001 //process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ---- ${PORT}`)
-})
